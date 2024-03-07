f9a67a280a0f9c818048bdd8b2c7ea3f2f1ffc37
Rahull87 <90816455+Rahull87@users.noreply.github.com>
2024-03-07 22:39:57 +0530

    Day 6

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int l1=nums1.size(),l2=nums2.size();
+        vector<int> ans;
+        
+        int arr[1001]={-1};
+        for(int i=0;i<l1;i++){
+            arr[nums1[i]]=1;
+        }
+        for(int i=0;i<l2;i++){
+            if(arr[nums2[i]]==1) {
+                ans.push_back(nums2[i]); 
+                arr[nums2[i]]=-1;
+            }
+        }
+        return ans;
+    }
+};
