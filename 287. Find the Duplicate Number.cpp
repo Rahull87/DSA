b641f35c4074eab77dffdc6b8439a2a17bb42aa3
Rahull87 <90816455+Rahull87@users.noreply.github.com>
2024-03-11 22:37:59 +0530

    Day 10

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int n = nums.size();
+        if(n<0)
+        {
+            return -1;
+        }
+        if(n<=2)
+        {
+            
+            return nums[0];
+        }
+        sort(nums.begin() , nums.end());
+        int ans ;
+        for(int i = 0 ; i<n ; i++)
+        {
+            if (nums[i] == nums[i+1])
+            {
+                ans = nums[i] ;
+            }
+        }
+        return ans;
+    }
+};
